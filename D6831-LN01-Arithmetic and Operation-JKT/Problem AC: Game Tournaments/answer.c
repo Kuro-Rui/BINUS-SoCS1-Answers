045e20adbb1b68d2135db0433ec3d5e821fb390f
Louis Dominic <83048873+Kuro-Rui@users.noreply.github.com>
2024-09-26 06:04:54 +0700

    Add more comment for knowledge ig idk

--- D6831-LN01-Arithmetic and Operation-JKT/Problem AC: Game Tournaments/answer.c
+++ D6831-LN01-Arithmetic and Operation-JKT/Problem AC: Game Tournaments/answer.c
@@ -5,6 +5,7 @@ int main() {
     scanf("%hu", &N);
 
     // Binary: 1 (1) -> 10 (2) -> 100 (4) -> 1000 (8)
+    // So 1ULL << N can be used to calculate 2^N
     unsigned long long outcomes = (1ULL << N) - 1;
     printf("%llu\n", outcomes);
     return 0;
