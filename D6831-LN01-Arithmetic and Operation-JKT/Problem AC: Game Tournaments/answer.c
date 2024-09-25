742ba625fe3d327453d7ef6fd3efb4a915d22baf
Louis Dominic <83048873+Kuro-Rui@users.noreply.github.com>
2024-09-26 05:55:49 +0700

    I feel like this is better

--- D6831-LN01-Arithmetic and Operation-JKT/Problem AC: Game Tournaments/answer.c
+++ D6831-LN01-Arithmetic and Operation-JKT/Problem AC: Game Tournaments/answer.c
@@ -4,7 +4,8 @@ int main() {
     unsigned short N;
     scanf("%hu", &N);
 
-    long long outcomes = (1LL << N) - 1;
-    printf("%lld\n", outcomes);
+    // Binary: 1 (1) -> 10 (2) -> 100 (4) -> 1000 (8)
+    unsigned long long outcomes = (1ULL << N) - 1;
+    printf("%llu\n", outcomes);
     return 0;
 }
