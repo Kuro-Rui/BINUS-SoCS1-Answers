55a9f82300e1958900c26fc54cd9c04c1121b20e
Louis Dominic <83048873+Kuro-Rui@users.noreply.github.com>
2024-10-06 08:58:13 +0700

    Secret code :O

--- Repetition/Problem AP: pigw kizq ixi siuc/answer.c
+++ Repetition/Problem AP: pigw kizq ixi siuc/answer.c
@@ -6,14 +6,16 @@ int main() {
     scanf("%hu", &T);
 
     for (int t = 1; t <= T; t++) {
+        // Try 15 18
         scanf("%hu %u", &N, &K);
 
         char S[N + 1];
+        // Secret code: pigwkizqixisiuc
         scanf("%s", S);
         printf("Case #%d: ", t);
 
         K %= 26;
-        for (int i = 0; i < N; i++) printf("%c", 'a' + ((S[i] - 'a' + K) % 26));
+        for (int i = 0; i < N; i++) printf("%c", 'a' + (S[i] - 'a' + K) % 26);
         printf("\n");
     }
     return 0;
