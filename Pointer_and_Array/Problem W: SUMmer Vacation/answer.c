174f9168838ce23f2774f3798bd7398928f9f887
Louis Dominic <83048873+Kuro-Rui@users.noreply.github.com>
2024-10-08 21:04:12 +0700

    I think this would be more readable

--- Pointer_and_Array/Problem W: SUMmer Vacation/answer.c
+++ Pointer_and_Array/Problem W: SUMmer Vacation/answer.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
 
 int main() {
-	unsigned short T, N;
-	scanf("%hu", &T);
-	for (int t = 1; t <= T; t++) {
+    unsigned short T, N;
+    scanf("%hu", &T);
+    for (int t = 1; t <= T; t++) {
         scanf("%hu", &N);
 
-		unsigned short A[N];
-		for (int i = 0; i < N; i++) scanf("%hu", &A[i]);
+        unsigned short A[N];
+        for (int i = 0; i < N; i++) scanf("%hu", &A[i]);
 
-		unsigned short coolFactors = 0;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-			    unsigned short check = 0;
-				for (int k = j + 1; k < N; k++) {
-					if (i != j && i != k && j != k && A[i] == A[j] + A[k]) {
-						coolFactors++;
-						check = 1;
-						break;
-					}
-				}
-				if (check) break;
-			}
-		}
-		printf("Case #%d: %hu\n", t, coolFactors);
-	}
-	return 0;
+        unsigned short coolFactors = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (i == j) continue;
+
+                unsigned short check = 0;
+                for (int k = j + 1; k < N; k++) {
+                    if (i != k && j != k && A[i] == A[j] + A[k]) {
+                        coolFactors++;
+                        check = 1;
+                        break;
+                    }
+                }
+                if (check) break;
+            }
+        }
+        printf("Case #%d: %hu\n", t, coolFactors);
+    }
+    return 0;
 }
